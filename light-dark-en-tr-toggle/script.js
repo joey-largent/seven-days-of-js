e992c2d83b61812b158f0ff3491ecbd3db0b5520
joey-largent <177733313+joey-largent@users.noreply.github.com>
2025-01-12 21:17:59 +0300

    finish default theme setting

--- light-dark-en-tr-toggle/script.js
+++ light-dark-en-tr-toggle/script.js
@@ -1,6 +1,10 @@
 function setDefaultTheme() {
     document.body.classList.add('light-mode');
     document.querySelector('#greeting').textContent = "Hello there, friend. Would you mind turning off the light?"
+
+    document.querySelector('#languageToggle').textContent = "Türkçe"
+
+    document.querySelector('#themeToggle').textContent = "Dark Mode"
 }
 
 setDefaultTheme();
