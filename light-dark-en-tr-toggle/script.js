7730b19bbc8e32006f33c63fff59e88a37bfbead
joey-largent <177733313+joey-largent@users.noreply.github.com>
2025-01-12 21:11:06 +0300

    set up basic files and structure, begin script

---
+++ light-dark-en-tr-toggle/script.js
@@ -0,0 +1,11 @@
+function setDefaultTheme() {
+    document.body.classList.add('light-mode');
+    document.body.getAttribute('#greeting')
+}
+
+
+toggleColorTheme()
+
+toggleLanguage()
+
+toggleText()
