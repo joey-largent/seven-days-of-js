94b9ed8c5397c18577f929fc8c47b7533e4527fe
joey-largent <177733313+joey-largent@users.noreply.github.com>
2025-01-13 11:29:01 +0300

    complete longform code and app works. vay be

--- README.md
+++ README.md
@@ -1,1 +1,14 @@
 # seven-days-of-js
+The purpose of this self-guided project is to review and practice fundamental JS concepts like conditionals, arrays, objects, functions, dom manipulation and events.
+
+Because I usually spend a lot of time on the design, I will have ai generate simple html and css templates for me (because I am very comfortable in these areas already) so that I can focus entirely on making the app functional. If I have time, I will later come back and make the design my own
+
+The content will be mostly fun and playful so that my daughters can enjoy interacting with them. Since they only speak Turkish, the language of the apps will be in Turkish primarily with a UI switch to English
+
+Day One: Light/Dark Mode + English/Turkish Toggle (for future use)
+Day Two: Personalized Funny Greeting Generator
+Day Three: Interactive Istanbul Sounds
+Day Four: Picture matching game (can change the size of the grid to increase difficulty)
+Day Five: Silly Story Generator (you can input who, with who, where, at what time, and what are they doing)
+Day Six: a maze where at the end a scary image pops up on the screen with a monster sound
+Day Seven: Fortune teller (ask any yes or no question and it generates a random vague answer)
--- light-dark-en-tr-toggle/script.js
+++ light-dark-en-tr-toggle/script.js
@@ -1,17 +1,76 @@
 function setDefaultTheme() {
     document.body.classList.add('light-mode');
-    document.querySelector('#greeting').textContent = "Hello there, friend. Would you mind turning off the light?"
+    document.body.classList.add('english');
+    document.querySelector('.greeting').textContent = "Hello there, friend. Would you mind turning off the light?"
 
-    document.querySelector('#languageToggle').textContent = "Türkçe"
+    document.querySelector('.languageToggle').textContent = "Türkçe"
 
-    document.querySelector('#themeToggle').textContent = "Dark Mode"
+    document.querySelector('.themeToggle').textContent = "Dark Mode"
 }
 
 setDefaultTheme();
 
 
-toggleColorTheme()
+const themeToggle = document.querySelector('.themeToggle');
+const languageToggle = document.querySelector('.languageToggle')
+const container = document.querySelector('.container');
 
-toggleLanguage()
+function toggleColorTheme() {
+    if (document.body.classList.contains('light-mode')) {
+        document.body.classList.remove('light-mode');
+        document.body.classList.add('dark-mode');
+        document.querySelector('.themeToggle').textContent = 
+            document.body.classList.contains('english') ? "Light Mode" : "Açık Mod";
+        document.querySelector('.greeting').textContent =
+            document.body.classList.contains('english') 
+                ? "Wow, it's dark in here. Could you turn the lights on please?" 
+                : "Vay be burası çok karanlık. Işığı açsana olur mu?";
+    } else {
+        document.body.classList.remove('dark-mode');
+        document.body.classList.add('light-mode');
+        document.querySelector('.themeToggle').textContent =
+            document.body.classList.contains('english') ? "Dark Mode" : "Koyu Mod";
+        document.querySelector('.greeting').textContent = 
+            document.body.classList.contains('english')
+                ? "Hello there, friend. Would you mind turning off the light?"
+                : "Merhaba, dostum. Işığı kapatsana olur mu?";
+    }
+};
 
-toggleText()
+themeToggle.addEventListener('click', (event) => { 
+    toggleColorTheme();
+});
+
+function toggleLanguage() {
+    if (document.body.classList.contains('english')) {
+        document.body.classList.remove('english')
+        document.body.classList.add('turkish')
+
+        if (document.body.classList.contains('light-mode')) {
+            document.querySelector('.themeToggle').textContent = "Koyu Mod"
+            document.querySelector('.greeting').textContent = "Merhaba, dostum. Işığı kapatsana olur mu?" 
+        }   else {
+            document.querySelector('.themeToggle').textContent = "Açık Mod";
+            document.querySelector('.greeting').textContent = "Vay be burası çok karanlık. Işığı açsana olur mu?";
+        }
+
+        document.querySelector('.languageToggle').textContent = "English"
+    } else {
+        document.body.classList.remove('turkish');
+        document.body.classList.add('english');
+    
+        if (document.body.classList.contains('light-mode')) {
+          document.querySelector('.themeToggle').textContent = "Dark Mode";
+          document.querySelector('.greeting').textContent = "Hello there, friend. Would you mind turning off the light?";
+        } else {
+          document.querySelector('.themeToggle').textContent = "Light Mode";
+          document.querySelector('.greeting').textContent = "Wow, it's dark in here. Could you turn the lights on please?";
+        }
+    
+        document.querySelector('.languageToggle').textContent = "Türkçe";
+      }
+}
+
+languageToggle.addEventListener('click', (event) => { 
+    toggleLanguage();
+});
